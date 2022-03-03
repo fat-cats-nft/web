f8e7c0be1fc39aa2ce294d60e2b257c53a07e73f
John KM Jung <min85da@gmail.com>
2022-03-02 19:14:11 -0500

    add todo

--- components/wallet/wallet.js
+++ components/wallet/wallet.js
@@ -1,28 +1,30 @@
+import { Flex } from '@chakra-ui/react';
 import { useContext } from 'react';
 
 import { WalletContext } from '../contexts';
 import commonStyles from '../common.module.css';
 import walletStyles from './wallet.module.css';
 
+// TODO: refactor using chakra-ui mdoal
 export default function DisconnectWallet({ showDisconnectWallet, setDisconnectWallet }) {
   const { setShowDisconnectWallet, address } = useContext(WalletContext);
-  const showHideClassName = showDisconnectWallet ? { display: 'block' } : { display: 'none' };
 
   return (
-    <div className={walletStyles.modal} style={showHideClassName}>
+    <div
+      className={walletStyles.modal}
+      style={{ display: showDisconnectWallet ? 'block' : 'none' }}
+    >
       <div className={walletStyles.main}>
         <div className={walletStyles.container}>Address:</div>
         <div className={walletStyles.container}>{address}</div>
-        <div className={walletStyles.container}>
+        <Flex className={walletStyles.container}>
           <button className={commonStyles.walletBtn} onClick={() => setShowDisconnectWallet(false)}>
             Close
           </button>
-        </div>
-        <div className={walletStyles.container}>
           <button className={commonStyles.walletBtn} onClick={() => setDisconnectWallet(true)}>
             Disconnect
           </button>
-        </div>
+        </Flex>
       </div>
     </div>
   );
