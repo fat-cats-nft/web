fb6c1330fa6a5f9960644745db49c011c9a2e8f2
John KM Jung <min85da@gmail.com>
2022-03-05 16:46:59 -0500

    refactor to wallet to Modal

--- components/wallet/wallet.js
+++ components/wallet/wallet.js
@@ -1,31 +1,50 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useClipboard,
+} from '@chakra-ui/react';
 import { useContext } from 'react';
 
 import { WalletContext } from '../contexts';
 import commonStyles from '../common.module.css';
 import walletStyles from './wallet.module.css';
 
-// TODO: refactor using chakra-ui mdoal
 export default function DisconnectWallet({ showDisconnectWallet, setDisconnectWallet }) {
   const { setShowDisconnectWallet, address } = useContext(WalletContext);
+  const { hasCopied, onCopy } = useClipboard(address);
+
+  const closeWallet = () => {
+    setShowDisconnectWallet(false);
+  };
 
   return (
-    <div
-      className={walletStyles.modal}
-      style={{ display: showDisconnectWallet ? 'block' : 'none' }}
-    >
-      <div className={walletStyles.main}>
-        <div className={walletStyles.container}>Address:</div>
-        <div className={walletStyles.container}>{address}</div>
-        <Flex className={walletStyles.container}>
-          <button className={commonStyles.walletBtn} onClick={() => setShowDisconnectWallet(false)}>
-            Close
-          </button>
-          <button className={commonStyles.walletBtn} onClick={() => setDisconnectWallet(true)}>
+    <Modal isOpen={showDisconnectWallet} onClose={closeWallet}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Address</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {/* TODO: add clipboard icon?  */}
+          <div style={{ cursor: 'pointer' }} onClick={onCopy}>
+            {address}
+          </div>
+          {hasCopied && (
+            <div style={{ position: 'absolute', fontSize: '14px', color: 'grey' }}>Copied</div>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <Button variant="ghost" onClick={() => setDisconnectWallet(true)}>
             Disconnect
-          </button>
-        </Flex>
-      </div>
-    </div>
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 }
